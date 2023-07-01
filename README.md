be027f355dd5337bbe7d2f9522bfe54fc81b06da
jasonh34 <105529595+jasonh34@users.noreply.github.com>
2023-07-01 17:20:28 -0400

    update generator, readme

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 # my_first_number_guesser.py
 
-My first number guesser game in Python, only written in a few months!
+My first number guessing game in Python!
 
 Inspired by [my_first_calculator.py](https://github.com/AceLewis/my_first_calculator.py) by [AceLewis](https://github.com/AceLewis).
-
-Yes, this is a joke project.
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+(Yes, this is a joke project.)
--- my_first_number_guesser_generator.py
+++ my_first_number_guesser_generator.py
@@ -1,11 +1,17 @@
+# This is the generator file for my_first_number_guesser.py
+
+# Customizable min and max values for the number guesser, can support negative values
+# Just don't overdo it otherwise your computer *may* [figuratively] explode
+# (not responsibe if your computer actually explodes)
 min = 1
-max = 100
+max = 101
 
 file = open("my_first_number_guesser.py", "w")
 
 file.write("import random\n\n" +
 
-    "#TODO: Add support for decimal/floating point numbers, and numbers from -1000 to 1000\n\n" +
+    # for comedic purposes
+    "#TODO: Add support for decimal/floating point numbers, and numbers greater than 101\n\n" +
 
     "computer_number = random.randint(" + str(min) + ", " + str(max) + ")\n\n" +
     
@@ -25,6 +31,8 @@ for i in range(min, max + 1):
             file.write("\t\t\t\tprint(\"\\nYou guessed it! My number is \" + str(computer_number) + \"!\")\n")
             file.write("\t\t\t\tbreak\n")
         else:
+            # When the code is generated, it may look a bit weird since Too High! is on top,
+            # however I left it like this because I think it's more funnier this way
             if j > i:
                 file.write("\t\t\t\tprint(\"\\nToo low!\")\n")
             else:
